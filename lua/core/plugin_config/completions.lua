9d9af7bf339df7b28a5b0e5e57b2504f3cca973c
Ole-Martin Michelsen <om.michelsen@gmail.com>
2024-02-28 14:49:29 +0100

    Formatting and zig lsp

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -14,8 +14,8 @@ cmp.setup({
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
     end,
-  },  
-  sources = cmp.config.sources({  
+  },
+  sources = cmp.config.sources({
     { name = 'nvim_lsp'},
     { name = 'luasnip'},
   }, {
--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,36 +1,51 @@
-require("mason").setup();
-local lsp_config = require("lspconfig");
+require("mason").setup()
+local lsp_config = require("lspconfig")
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "svelte", "gopls", "html", 'eslint', 'jsonls', 'cssls' },
-  automatic_installation = true,
+	ensure_installed = {
+		"lua_ls",
+		"svelte",
+		"gopls",
+		"html",
+		"biome",
+		"jsonls",
+		"cssls",
+		"rust_analyzer",
+		"zls",
+		"tsserver",
+	},
+	automatic_installation = true,
 })
 
-local on_attach = function(_,_)
-  vim.keymap.set('n', '<Space>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<Space>ca', vim.lsp.buf.code_action, {})
+local on_attach = function(_, _)
+	vim.keymap.set("n", "<Space>rn", vim.lsp.buf.rename, {})
+	vim.keymap.set("n", "<Space>ca", vim.lsp.buf.code_action, {})
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require('lspconfig').biome.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lsp_config["biome"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-
-require('lspconfig').lua_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lsp_config["rust_analyzer"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 lsp_config["gopls"].setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+lsp_config["zls"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 lsp_config["zls"].setup({
@@ -39,26 +54,26 @@ lsp_config["zls"].setup({
 })
 
 lsp_config["dartls"].setup({
-  on_attach = on_attach,
-  settings = {
-    dart = {
-      analysisExlcudedFolders = {
-        vim.fn.expand("HOME/AppData/Local/Pub/Cache"),
-        vim.fn.expand("Home/.pub-cache"),
-        vim.fn.expand("/opt/homebrew"),
-        vim.fn.expand("$HOME/tools/flutter"),
-      },
-    }
-  },
+	on_attach = on_attach,
+	settings = {
+		dart = {
+			analysisExlcudedFolders = {
+				vim.fn.expand("HOME/AppData/Local/Pub/Cache"),
+				vim.fn.expand("Home/.pub-cache"),
+				vim.fn.expand("/opt/homebrew"),
+				vim.fn.expand("$HOME/tools/flutter"),
+			},
+		},
+	},
 })
 
-lsp_config['tsserver'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-lsp_config['cssls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lsp_config["tsserver"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+lsp_config["cssls"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 require("fidget").setup({})
