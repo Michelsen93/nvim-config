d80c3c69ca8f41e0149a478a969cf7721307c160
Ole-Martin Michelsen <om.michelsen@gmail.com>
2025-10-31 15:45:26 +0100

    if err for go

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -42,6 +42,20 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    -- Normal mode mapping: <leader>ie
+    vim.keymap.set("n", "<Space>ie", function()
+      local line = "if err != nil {"
+      local ret_line = "\treturn"
+      local end_brace = "}"
+      local row = vim.api.nvim_win_get_cursor(0)[1]
+      vim.api.nvim_buf_set_lines(0, row, row, false, { line, ret_line, end_brace })
+    end, { buffer = true, desc = "Insert if err != nil { return }" })
+  end,
+})
+
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
